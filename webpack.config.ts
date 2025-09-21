41eeb5fabfac5cb9407cd4de43f7b1d8d3a63e08
Dan McMyler <danmcmyler@gmail.com>
2025-09-21 21:56:37 +0100

    build: added webpack

---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import type { Configuration } from "webpack";
+
+const config: Configuration = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    clean: true,
+    publicPath: "/fpl-planner/"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      inject: "body"
+    })
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+    hot: true
+  }
+};
+
+export default config;
